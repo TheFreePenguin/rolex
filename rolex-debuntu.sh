01008e3506d1f2e199c7f137227e9e95e6481b63
TheFreePenguin <81381555+TheFreePenguin@users.noreply.github.com>
2021-12-27 15:58:59 -0800

    Update rolex-debuntu.sh

--- rolex-debuntu.sh
+++ rolex-debuntu.sh
@@ -6,7 +6,7 @@ then
   echo "Step 1: Update Repositories"
   sudo apt update
   echo "Step 2: Install Dependencies"
-  sudo apt install -y git python3-pip python3-setuptools python3-wheel python3-dev pkg-config libcairo2-dev gtk-update-icon-cache p7zip-full desktop-file-utils xdg-utils libgirepository1.2-dev gir1.2-gtk-3.0
+  sudo apt install -y git python3-pip python3-setuptools python3-wheel python3-dev pkg-config libcairo2-dev gtk-update-icon-cache p7zip-full desktop-file-utils xdg-utils libgirepository1.0-dev gir1.2-gtk-3.0
   echo "Step 3: Install Wine"
   wget https://pastebin.com/raw/5SeVb005 -O /tmp/grapejuice-wine-tkg.py
   python3 /tmp/grapejuice-wine-tkg.py
